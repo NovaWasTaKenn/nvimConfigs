9d573c5ba17832d20e7d2735bcf588db1a202bed
quentin <quentin.le-nestour@outlook.com>
2025-10-14 23:59:10 +0200

    added mini-nvim modules

--- plugins/mini.nix
+++ plugins/mini.nix
@@ -1,7 +1,15 @@
 {
-  #config.vim.mini = {
-        #git.enable = true;
-        #icon.enable = true;
-        #};
-
+  config.vim.mini = {
+    surround.enable = true;
+    comment.enable = true;
+    splitjoin.enable = true;
+    operators.enable = true;
+    notify.enable = true;
+    map.enable = true;
+    indentscope.enable = true;
+    #cursorword.enable = true;
+    completion.enable = true;
+    basics.enable = true;
+    ai.enable = true;
+  };
 }
