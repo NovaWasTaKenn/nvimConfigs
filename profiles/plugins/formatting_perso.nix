8c25050e735b4412cdd9ab881f3fb183621a8a47
quentin <quentin.le-nestour@outlook.com>
2025-11-14 22:34:10 +0100

    fix

--- profiles/plugins/formatting_perso.nix
+++ profiles/plugins/formatting_perso.nix
@@ -1,6 +1,6 @@
 {
   config.vim.languages.enableFormat = true;
-  config.lsp.formatOnSave = true;
+  config.vim.lsp.formatOnSave = true;
   config.vim.options = {
         tabstop = 2;
         shiftwidth = 2;
